742938118dec2aec84c6972389016b2620974bef
raspacee <khapungbj84@gmail.com>
2024-02-02 16:54:06 +0545

    Create huffman tree

--- huffman.c
+++ huffman.c
@@ -19,78 +19,59 @@ int main() {
         frequency[index]++;
     }
 
-    int heap_end;
-    Node* heap = create_heap(ALPHABETS_LEN, &heap_end);
+    PQueue *q = initialize_pqueue(30);
+
     for (int i=0; i<ALPHABETS_LEN; i++) {
         if (frequency[i] > 0) {
-            insert_heap(heap, &heap_end, i + 97, frequency[i], NULL, NULL);
+            Node *tmp = malloc(sizeof(Node));
+            tmp->ch = i + 97;
+            tmp->freq = frequency[i];
+            tmp->left = NULL;
+            tmp->right= NULL;
+            enqueue_pqueue(q, tmp);
         }
     }
 
-    while (heap_end >= 2) {
-        Node a = get_min(heap, &heap_end);
-        Node b = get_min(heap, &heap_end);
-        insert_heap(heap, &heap_end, 0, a.freq + b.freq, &a, &b);
+    while (get_size_pqueue(q) > 1) {
+        Node *left_node = dequeue_pqueue(q);
+        Node *right_node = dequeue_pqueue(q);
+        Node *new_node = malloc(sizeof(Node));
+        new_node->ch = 0;
+        new_node->freq = left_node->freq + right_node->freq;
+        new_node->left = left_node;
+        new_node->right = right_node;
+        enqueue_pqueue(q, new_node);
     }
 
-    Stack stack;
-    stack.tos = -1;
-    traverse(&heap[0], &stack);
+    int arr[20];
+    int top = 0;
+    print_codes(q->queue[0], arr, top);
 
-    free(heap);
+    free(q);
     fclose(fp);
     return 0;
 }
 
-void traverse(Node *node, Stack *stack) {
-    // if (node == NULL) return;
-    // if (node->left == NULL && node->right == NULL) {
-    //     char code[30];
-    //     get_string(stack, code);
-    //     printf("Symbol %c has code %s\n", node->ch, code);
-    // } else {
-    //     push_stack(stack, '0');
-    //     traverse(node->left, stack);
-    //     pop_stack(stack);
+void print_codes(Node *root, int arr[], int top) 
+  
+{ 
+    if (root->left) { 
+        arr[top] = 0; 
+        print_codes(root->left, arr, top + 1); 
+    } 
+  
+    if (root->right) { 
+        arr[top] = 1; 
+        print_codes(root->right, arr, top + 1); 
+    } 
+  
+    if (root->ch > 0) { 
+        printf("code for %c is : ", root->ch); 
 
-    //     push_stack(stack, '1');
-    //     traverse(node->right, stack);
-    //     pop_stack(stack);
-    // }
-    if (node == NULL) return;
-    printf("%c\n", node->ch);
-    traverse(node->left, stack);
-    traverse(node->right, stack);
-}
-
-void push_stack(Stack *stack, char ch) {
-    if (stack->tos >= STACK_SIZE) {
-        printf("ERROR(add_stack): stack size limit exceeded.\n");
-        exit(-1);
-    }
-    stack->tos = stack->tos + 1;
-    stack->stk[stack->tos] = ch;
-}
-
-char pop_stack(Stack *stack) {
-    if (stack->tos >= 0) {
-        int tmp = stack->tos;
-        stack->tos = stack->tos - 1;
-        return stack->stk[tmp];
-    } else {
-        printf("Stack is empty cannot pop.\n");
-        return;
-    }
-}
-
-char peek_stack(Stack *stack) {
-    return stack->stk[stack->tos - 1];
-}
-
-void get_string(Stack *stack, char *code) {
-    int tmp = stack->tos;
-
-    for (tmp; tmp >= 0; tmp--) {
-        code[tmp] = stack->stk[tmp];
-    }
-}
+        for (int i=0; i<top; i++) {
+            printf("%d", arr[i]);
+        }
+        printf("\n");
+    } 
+} 
+  
--- huffman.h
+++ huffman.h
@@ -8,16 +8,6 @@ typedef struct Node_ {
 } Node;
 
 #define ALPHABETS_LEN 26
-#define STACK_SIZE 20
 
-typedef struct Stack_ {
-    char stk[STACK_SIZE];
-    int tos;
-} Stack;
-
-void traverse(Node *node, Stack *stack);
-void push_stack(Stack *stack, char ch);
-char pop_stack(Stack *stack);
-char peek_stack(Stack *stack);
-void get_string(Stack *stack, char *code);
+void print_codes(Node *node, int arr[], int top);
 #endif
